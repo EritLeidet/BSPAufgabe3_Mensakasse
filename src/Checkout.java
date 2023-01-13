8d5829d469a5de5a8e76024236a015a1fd01c264
Jessica <Jessica.broese@haw-hamburg.de>
2023-01-13 21:06:23 +0100

    decreaseQueueLength() nach hinten

--- src/Checkout.java
+++ src/Checkout.java
@@ -40,8 +40,8 @@ public class Checkout {
         }
         finally {
             System.out.println(threadName + " has payed.");
-            decreaseQueueLength();
             mutex.unlock();
+            decreaseQueueLength();
         }
     }
 
